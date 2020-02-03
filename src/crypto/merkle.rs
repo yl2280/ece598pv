b6efbdfc575e206ebd5cf4f360c0ee9af0fbc7bb
李怿然 <liyiran@liyirandeMacBook-Pro-2.local>
2020-02-03 16:20:13 -0600

    assign2

--- src/crypto/merkle.rs
+++ src/crypto/merkle.rs
@@ -1,29 +1,177 @@
 use super::hash::{Hashable, H256};
+use ring::{digest};
+use ring::digest::Algorithm;
+
 
 /// A Merkle tree.
 #[derive(Debug, Default)]
+struct Node {
+    val: H256,
+    l: Option<Box<Node>>,
+    r: Option<Box<Node>>,
+}
+
+
+#[derive(Debug, Default)]
 pub struct MerkleTree {
+    root: Option<Box<H256>>,
+    // blocks: Vec<H256>,
+    tree: Option<Box<Node>>,
+ 
+
 }
 
 impl MerkleTree {
     pub fn new<T>(data: &[T]) -> Self where T: Hashable, {
-        unimplemented!()
-    }
+        // unimplemented!()
+        if data.len() == 0{
+            return MerkleTree{root: None, tree: None};
+        }
 
+        if data.len() == 1{
+            let root = data[0].hash();
+            let tree:Node = Node{val: root, l:None, r : None};
+            return MerkleTree{root: Some(Box::new(root)), tree: Some(Box::new(tree))};
+        }
+
+        let mut vec = Vec::with_capacity(data.len());
+
+        for i in data{
+            let leaf = Node{val: i.hash(), l: None, r: None};
+            vec.push(leaf);
+        }
+
+        
+
+        if vec.len()%2 == 1{
+            let le = vec.len()-1;
+            let odd = &vec[le];
+            let n = odd.val;
+            let last = Node{val: odd.val, l: None, r:None};
+            vec.push(last);
+        }
+
+        while vec.len() > 1{
+            let mut vec_ = Vec::new();
+            while vec.len()!= 0{
+                if vec.len() == 1{
+                    vec_.push(vec.remove(0));
+                }
+                else{
+                    let l1 = vec.remove(0);
+                    let r1 = vec.remove(0);
+                    let l2 = (l1.val).as_ref();
+                    let r2 = (r1.val).as_ref();
+                    let parent = [&l2[..], &r2[..]].concat();
+                    let parentH = digest::digest(&digest::SHA256, &parent);
+                    let val = <H256>::from(parentH);
+                    let newNode:Node = Node{val,  l: Some(Box::new(l1)), r: Some(Box::new(r1))};
+                    // let newNode = Some(Box::new(node))
+                    vec_.push(newNode);
+                }
+            }
+
+            vec = vec_;
+        }
+
+        let t = vec.remove(0);
+        let r = t.val;
+
+        return MerkleTree{root: Some(Box::new(r)), tree: Some(Box::new(t))};
+
+
+    }
     pub fn root(&self) -> H256 {
-        unimplemented!()
+        // unimplemented!()
+        let ret = self.root.as_ref();
+        let r = ret.unwrap();
+        return **r;
     }
 
     /// Returns the Merkle Proof of data at index i
     pub fn proof(&self, index: usize) -> Vec<H256> {
-        unimplemented!()
+        // unimplemented!()
+        // let cur = self.tree;
+        let mut vec = Vec::new();
+        // let merkle = self.clone();
+        let mut cur = (self.tree).as_ref();
+        let mut curr = (cur).unwrap();
+        let mut count = 2;
+        let i = index + 1;
+        let mut j = 0;
+
+        while cur.is_some(){
+            // let curr = &*(cur.unwrap());
+            j += 1;
+            let left = &curr.l;
+            let right = &curr.r;
+            if left.is_none() || right.is_none(){
+                // vec.push(curr.val);
+                break;
+            }
+
+            if i < count{
+                let tran = &((curr.l).as_ref().unwrap());
+                cur = Some(tran);
+
+                // cur = &(curr.l.as_ref());
+                let sibiling = (curr.r.as_ref()).unwrap();
+                let sibV = sibiling.val;
+                vec.push(sibV);
+                curr = ((curr.l).as_ref()).unwrap();
+            }
+
+            else{
+                let tran = &((curr.r).as_ref().unwrap());
+                cur = Some(tran);
+                // cur = curr.r;
+            
+                // cur = &(curr.r.as_ref());
+                let sibiling = (curr.l.as_ref()).unwrap();
+                let sibV = sibiling.val;
+                vec.push(sibV);
+                curr = (curr.r.as_ref()).unwrap();
+            }
+
+            count *= 2;
+        }
+        return vec;
+
     }
 }
 
 /// Verify that the datum hash with a vector of proofs will produce the Merkle root. Also need the
 /// index of datum and `leaf_size`, the total number of leaves.
 pub fn verify(root: &H256, datum: &H256, proof: &[H256], index: usize, leaf_size: usize) -> bool {
-    unimplemented!()
+    // unimplemented!()
+    let mut i = index + 1;
+    let mut ret = datum.clone();
+    let mut vec = Vec::from(proof.clone());
+    while vec.len() > 0{
+        if i%2 == 1{
+            let l1 = ret;
+            let r1 = vec.remove(vec.len()-1);
+            let l2 = l1.as_ref();
+            let r2 = r1.as_ref();
+            let parent = [&l2[..], &r2[..]].concat();
+            let parentH = digest::digest(&digest::SHA256, &parent);
+            ret = <H256>::from(parentH);
+            i = (i+1)/2;
+        } 
+
+        else{
+            let r1 = ret;
+            let l1 = vec.remove(vec.len()-1);
+            let l2 = l1.as_ref();
+            let r2 = r1.as_ref();
+            let parent = [&l2[..], &r2[..]].concat();
+            let parentH = digest::digest(&digest::SHA256, &parent);
+            ret = <H256>::from(parentH);
+            i /= 2;
+        }
+    }
+
+    return *root == ret;
 }
 
 #[cfg(test)]
@@ -45,6 +193,9 @@ mod tests {
         let input_data: Vec<H256> = gen_merkle_tree_data!();
         let merkle_tree = MerkleTree::new(&input_data);
         let root = merkle_tree.root();
+        let a = input_data[0].hash();
+        let b = input_data[1].hash();
+
         assert_eq!(
             root,
             (hex!("6b787718210e0b3b608814e04e61fde06d0df794319a12162f287412df3ec920")).into()
@@ -61,8 +212,10 @@ mod tests {
     #[test]
     fn proof() {
         let input_data: Vec<H256> = gen_merkle_tree_data!();
+        // println!("{:?}", input_data);
         let merkle_tree = MerkleTree::new(&input_data);
         let proof = merkle_tree.proof(0);
+        // println!("{:?}", proof);
         assert_eq!(proof,
                    vec![hex!("965b093a75a75895a351786dd7a188515173f6928a8af8c9baa4dcff268a4f0f").into()]
         );
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,18 +1,48 @@
 use serde::{Serialize,Deserialize};
 use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use std::slice;
+use bincode::{serialize, deserialize};
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Transaction {
+    input: std::string::String,
+    output: std::string::String,
+    signature: std::string::String 
 }
+
+// impl Transaction {
+//     fn signed(&mut self, sig: *str) -> () {
+//         self.signature = true
+//     }
+// }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    //unimplemented!()
+   
+    // t.signature = true;
+
+    let t_s = serialize(&t).unwrap();
+    let sig = key.sign(&t_s);
+    return sig;
+
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+    //unimplemented!()
+    let t_s = serialize(&t).unwrap();
+    // let ret = public_key.verify(slice, signature);
+    let peer_public_key_bytes = public_key.as_ref();
+    let peer_public_key = ring::signature::UnparsedPublicKey::new(&ring::signature::ED25519, peer_public_key_bytes);
+    // signature::verify()
+    let ret = peer_public_key.verify(&t_s, signature.as_ref());
+    // try!(peer_public_key.verify(&t_s, signature.as_ref()));
+    if(ret == Ok(())) { return true;}
+    else {return false;}
+
 }
 
 #[cfg(any(test, test_utilities))]
@@ -21,8 +51,25 @@ mod tests {
     use crate::crypto::key_pair;
 
     pub fn generate_random_transaction() -> Transaction {
-        Default::default()
+        // Default::default()
         //unimplemented!()
+        let input: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+
+        let output: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+        let s = "";
+        let signature = s.to_string();
+        // signature = signature.to_string();
+
+        let ret = Transaction{input, output, signature};
+
+        return ret;
+
     }
 
     #[test]
